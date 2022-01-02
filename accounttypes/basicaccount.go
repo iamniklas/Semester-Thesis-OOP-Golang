7bfe7ac485e9abea52a42f2858bc659636b3e544
iamniklas <niklas.englmeier@web.de>
2022-01-02 17:29:16 +0100

    Update basicaccount.go

--- accounttypes/basicaccount.go
+++ accounttypes/basicaccount.go
@@ -18,13 +18,15 @@ func (basicAccount BasicAccount) GetAccountInfo() string {
 
 func (basicAccount *BasicAccount) Withdraw(amount float64, transferToOtherAccount bool) int {
 	amount = amount * 0.95
-	if amount > 0.0 {
-		basicAccount.AccountBalance = basicAccount.AccountBalance - amount
-		basicAccount.Account.AddTransaction(transferToOtherAccount, -amount)
-		return 0
-	} else {
+	if transferToOtherAccount && amount > basicAccount.AccountBalance {
 		return 1
 	}
+	if amount <= 0.0 {
+		return 2
+	}
+	basicAccount.AccountBalance = basicAccount.AccountBalance - amount
+	basicAccount.Account.AddTransaction(transferToOtherAccount, -amount)
+	return 0
 }
 
 func (basicAccount *BasicAccount) Deposit(amount float64, transferToOtherAccount bool) int {
